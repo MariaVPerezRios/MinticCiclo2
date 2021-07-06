15ca185fc446aaa05f8dc8b1029a29b596f3f123
Maria Victoria Perez Rios <mariavictoria.perezrios@yahoo.com>
2021-07-06 18:24:26 -0500

    ejercicios

---
+++ Ejercicio_1/src/saludoenconsola.java
@@ -0,0 +1,13 @@
+public class saludoenconsola {
+    public static void main(String[] args) {
+        var nombre= "Maria Victoria";
+
+        var resultado= saludo(nombre); 
+
+        System.out.println(resultado);
+    } 
+    public static String saludo(string nombre){
+        return "Hola" + nombre + "!";
+        
+    }
+}
